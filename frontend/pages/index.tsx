4eca49b30a3b5c84e99c55f86b9588d2b25df32d
Matej Kuka <mate1778@easv365.dk>
2023-03-23 21:18:54 +0100

    dsfsdfd

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -8,9 +8,9 @@ export default function Home() {
         <div>
             <LandpageAlfa/>
             <div className={"text-center"}>
-                <h2 className={"lg:text-6xl md:text-5xl text-4xl pt-12"}>Ako ti vie Vizio pomôcť?</h2>
+                <h2 className={"lg:text-6xl md:text-5xl text-4xl pt-12"}>Ako ti  vie Vizio pomôcť?</h2>
                 <iframe
-                    className={"mx-auto my-12 lg:w-[800px] lg:h-[600px] md:w-[600px] md:h-[600px] w-11/12 h-[350px]"}
+                    className={"mx-auto my-12 lg:w-[800px] lg:h-[600px] md:w-[600px] md:h-[800px] w-11/12 h-[500px]"}
                     src="https://docs.google.com/forms/d/e/1FAIpQLSdjBA0z5V87X37ZPaU_MwJtThHFa_CJ8eAtNz_n-OgMXConCw/viewform?embedded=true" frameBorder="0" marginHeight={0} marginWidth={0}>Načítava sa…
                 </iframe>
             </div>
